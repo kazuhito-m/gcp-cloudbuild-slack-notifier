2acc9c8847581f59dd8b3ca3c5d9e95cde805a75
kazuhito_m <kazuhito.sumpic@gmail.com>
2019-11-21 23:55:58 +0900

    first commit: まず、Goプロジェクトの「基本の形」を作る。

---
+++ go.mod
@@ -0,0 +1,3 @@
+module xyz/gcp-cloudbuild-slack-notifier
+
+require github.com/kazuhito-m/gcp-cloudbuild-slack-notifier master
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "fmt"
+  "github.com/kazuhito-m/gcp-cloudbuild-slack-notifier"
+)
+ 
+func main() {
+    fmt.Printf("%v\n", uuid.Must(uuid.NewV4()));
+}
