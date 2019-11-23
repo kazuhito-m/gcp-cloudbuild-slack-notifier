3854aa46da86242eab35c58cb7d7b3607835e4be
kazuhito_m <kazuhito.sumpic@gmail.com>
2019-11-23 23:12:52 +0900

    implement: ファイル分割して実装できることを確認。

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
   "fmt"
   "github.com/satori/go.uuid"
+  "kazuhito-m/gcp-cloudbuild-slack-notifier/slack"
 )
  
 func main() {
-    fmt.Printf("%v\n", uuid.Must(uuid.NewV4()));
+	fmt.Printf("%v\n", uuid.Must(uuid.NewV4()))
+	slack.Sample()
 }
---
+++ slack/slack.go
@@ -0,0 +1,10 @@
+package slack
+
+import (
+	"fmt"
+)
+
+func Sample() string {
+	fmt.Printf("testtest\n")
+	return "これを返すことになっている。"
+}
