e949596c8958500d5fdfc81ee82063ad70adf500
kazuhito_m <kazuhito.sumpic@gmail.com>
2019-11-28 00:42:41 +0900

    refactor: テストの際にGoFmt掛かるようにしておく。

--- all_test.sh
+++ all_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+for i in $(find . -name '*.go') ; do
+  echo ${i}
+  go fmt ${i}
+done
+
 for i in $(find . -name '*_test.go') ; do
   go test -cover -v ${i}
 done
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-  "fmt"
-  "github.com/satori/go.uuid"
-  "kazuhito-m/gcp-cloudbuild-slack-notifier/slack"
+	"fmt"
+	"github.com/satori/go.uuid"
+	"kazuhito-m/gcp-cloudbuild-slack-notifier/slack"
 )
- 
+
 func main() {
 	fmt.Printf("%v\n", uuid.Must(uuid.NewV4()))
 	slack.Sample()
