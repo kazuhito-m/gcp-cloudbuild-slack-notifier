7eee2ea4573f2a06bf0c4270d033eefc4ea306d8
kazuhito_m <kazuhito.sumpic@gmail.com>
2019-12-08 23:11:44 +0900

    implmenet: CloudFunctionsに登録出来る形に変更。

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module kazuhito-m/gcp-cloudbuild-slack-notifier
 
-require github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
-
 go 1.13
--- handler.go
+++ handler.go
@@ -1,14 +1,17 @@
-package main
+package handler
 
 import (
 	"context"
 	"encoding/json"
 	"kazuhito-m/gcp-cloudbuild-slack-notifier/cloudbuild"
+	"kazuhito-m/gcp-cloudbuild-slack-notifier/config"
+	"kazuhito-m/gcp-cloudbuild-slack-notifier/notify"
 	"kazuhito-m/gcp-cloudbuild-slack-notifier/pubsub"
+	"kazuhito-m/gcp-cloudbuild-slack-notifier/slack"
 	"log"
 )
 
-func HelloFromPubSub(ctx context.Context, message pubsub.PubSubMessage) error {
+func PubSubHandlerForCloudBuild(ctx context.Context, message pubsub.PubSubMessage) error {
 	var decode_data cloudbuild.CloudBuildResult
 	if err := json.Unmarshal(message.Data, &decode_data); err != nil {
 		log.Fatal(err)
@@ -18,5 +21,26 @@ func HelloFromPubSub(ctx context.Context, message pubsub.PubSubMessage) error {
 	text := string(message.Data)
 	log.Println("取得できたData部:" + text)
 
+	config, ok := config.Load()
+	if !ok {
+		log.Fatalln("環境変数から設定が取得できませんでした。終了します。")
+		return nil
+	}
+	log.Println(config.SlackURL)
+
+	notify := notify.CreateNotify()
+
+	result := slack.SendNotify(notify, config)
+
+	log.Println(endInfo(result))
+
 	return nil
+}
+
+func endInfo(result bool) string {
+	resText := "失敗"
+	if result {
+		resText = "成功"
+	}
+	return "Slack通知が" + resText + "しました。"
 }
