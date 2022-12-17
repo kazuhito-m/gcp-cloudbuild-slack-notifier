7002f27999be4a620a2c96d23a1d483d5d9b6300
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-12-17 19:15:38 +0900

    refactor: コード上のバグが治ったので、テストスクリプトが簡素に。

--- all_test.sh
+++ all_test.sh
@@ -4,14 +4,4 @@ for i in $(find . -name '*.go') ; do
   go fmt ${i}
 done
 
-result=0
-for i in $(find . -name '*_test.go') ; do
-  go test -cover -v ${i}
-
-  r=$?
-  if [ ${r} -ne 0 ]; then
-    result=${r}
-  fi  
-done
-
-exit ${result}
+go test -cover -v ./...
