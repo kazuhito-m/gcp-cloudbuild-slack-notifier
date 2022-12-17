0fdc474b78df6e35a8dc2b5c7e7c09d8ea961294
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-12-17 18:03:53 +0900

    refactor: テストがコケるように。

--- all_test.sh
+++ all_test.sh
@@ -4,6 +4,14 @@ for i in $(find . -name '*.go') ; do
   go fmt ${i}
 done
 
+result=0
 for i in $(find . -name '*_test.go') ; do
   go test -cover -v ${i}
+
+  r=$?
+  if [ ${r} -ne 0 ]; then
+    result=${r}
+  fi  
 done
+
+exit ${result}
